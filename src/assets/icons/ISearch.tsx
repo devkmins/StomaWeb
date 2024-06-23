b2c33499b167dbe047406212b8449ba03c9b2927
Min <esspacetoM@gmail.com>
2024-06-24 00:16:51 +0900

    feat: 헤더 컴포넌트에서 사용될 ISearch 아이콘 추가

---
+++ src/assets/icons/ISearch.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const ISearch = ({ color = "#7f8c8d" }) => (
+  <svg
+    width="30"
+    height="30"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 16 16"
+    fill="currentColor">
+    <path
+      fill-rule="evenodd"
+      d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+      fill={color}
+      stroke={color}
+      strokeWidth="0.25"
+      clip-rule="evenodd"
+    />
+  </svg>
+);
+
+export default React.memo(ISearch);
