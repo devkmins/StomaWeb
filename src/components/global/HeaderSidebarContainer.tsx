12ee8e7f3dd1ad1c7b21ea70de784d504bbabd6d
Min <esspacetoM@gmail.com>
2024-06-24 00:18:36 +0900

    feat: Header 컴포넌트와 SideBar 컴포넌트를 통합한 HeaderSidebarContainer 컴포넌트 구현

---
+++ src/components/global/HeaderSidebarContainer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "@/components/global/Header";
+import SideBar from "@/components/global/SideBar";
+
+export default function HeaderSidebarContainer() {
+  return (
+    <div className="w-1/4 h-screen fixed p-1 border-r-2 boder-solid boder-black">
+      <Header />
+      <SideBar />
+    </div>
+  );
+}
