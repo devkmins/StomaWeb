1b4020eab45c7d32196a17565549717070801ccf
Min <esspacetoM@gmail.com>
2024-06-24 00:19:07 +0900

    feat: global layout에 HeaderSidebarContainer 컴포넌트 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import "./globals.css";
 
+import HeaderSidebarContainer from "@/components/global/HeaderSidebarContainer";
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html>
-      <body>{children}</body>
+      <body>
+        <HeaderSidebarContainer />
+        {children}
+      </body>
     </html>
   );
 }
