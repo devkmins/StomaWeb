466e77afba5c1971872b6add3de8638700f0c279
Min <esspacetoM@gmail.com>
2024-06-24 20:56:20 +0900

    feat: pathname에 따라 스크롤을 상단으로 이동시키거나 홈 페이지로 이동시키도록 설정

--- src/components/global/Header/HeaderLogo.tsx
+++ src/components/global/Header/HeaderLogo.tsx
@@ -1,17 +1,49 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
 
 import { ubuntu } from "@/assets/fonts/ubuntu";
+import { usePathname } from "next/navigation";
+
+import { scrollToTop } from "@/utils/scrollToTop";
 
 export default function HeaderLogo() {
+  const pathname = usePathname();
+
   return (
-    <div className="my-5 flex justify-center">
-      <Link href="/">
-        <Image width={50} height={50} src="/logo/logoCircle.png" alt="Logo" />
-      </Link>
-      <Link href="/" className="ml-2.5 flex items-center">
-        <span className={`${ubuntu.className} text-2xl`}>StomaWeb</span>
-      </Link>
+    <div>
+      {pathname === "/" ? (
+        <div className="cursor-pointer my-5 flex justify-center">
+          <div onClick={scrollToTop}>
+            <Image
+              width={50}
+              height={50}
+              src="/logo/logoCircle.png"
+              alt="Logo"
+            />
+          </div>
+          <div
+            onClick={scrollToTop}
+            className="cursor-pointer ml-2.5 flex items-center">
+            <span className={`${ubuntu.className} text-2xl`}>StomaWeb</span>
+          </div>
+        </div>
+      ) : (
+        <div className="my-5 flex justify-center">
+          <Link href="/">
+            <Image
+              width={50}
+              height={50}
+              src="/logo/logoCircle.png"
+              alt="Logo"
+            />
+          </Link>
+          <Link href="/" className="ml-2.5 flex items-center">
+            <span className={`${ubuntu.className} text-2xl`}>StomaWeb</span>
+          </Link>
+        </div>
+      )}
     </div>
   );
 }
