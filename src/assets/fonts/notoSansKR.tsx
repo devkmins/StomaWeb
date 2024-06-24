596c9f9401249cc6e63f71c1d9538c705703fc92
Min <esspacetoM@gmail.com>
2024-06-24 19:35:49 +0900

    feat: ubuntu, notoSansKR, playfairDisplay 폰트 파일 생성

---
+++ src/assets/fonts/notoSansKR.tsx
@@ -0,0 +1,6 @@
+import { Noto_Sans_KR } from "next/font/google";
+
+export const notoSansKR = Noto_Sans_KR({
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
+  subsets: ["latin"],
+});
