6b1281196ccebd2b2ac001b0ea6322d64a087833
Min <esspacetoM@gmail.com>
2024-06-25 23:48:07 +0900

    feat: 사이드바 컴포넌트에서 사용될 IArrowUp 아이콘 추가

---
+++ src/assets/icons/IArrowUp.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const IArrowUp = () => (
+  <svg
+    width="20"
+    height="20"
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke-width="1.5"
+    stroke="currentColor">
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="m4.5 15.75 7.5-7.5 7.5 7.5"
+    />
+  </svg>
+);
+
+export default React.memo(IArrowUp);
