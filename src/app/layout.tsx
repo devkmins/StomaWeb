387df821ab04657e708eb8f50702772de72cbbc0
Min <esspacetoM@gmail.com>
2024-06-26 03:34:49 +0900

    feat: global layout에 스타일 추가 및 ScrollToTopButton 컴포넌트 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,15 +2,17 @@ import React from "react";
 import "./globals.css";
 
 import HeaderSidebarContainer from "@/components/global/HeaderSidebarContainer/HeaderSidebarContainer";
+import ScrollToTopButton from "@/components/global/ScrollToTopButton/ScrollToTopButton";
 
 import { notoSansKR } from "@/assets/fonts/notoSansKR";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html>
-      <body className={`${notoSansKR.style}`}>
+      <body className={`${notoSansKR.style} h-screen relative`}>
         <HeaderSidebarContainer />
         {children}
+        <ScrollToTopButton />
       </body>
     </html>
   );
