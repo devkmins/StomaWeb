349107d6824588c16aee197ef07ad89fccc779fc
Min <esspacetoM@gmail.com>
2024-06-25 23:51:52 +0900

    HeaderSidebarContainer 스타일 추가 및 주석 추가

--- src/components/global/HeaderSidebarContainer.tsx
+++ src/components/global/HeaderSidebarContainer.tsx
@@ -4,9 +4,10 @@ import SideBar from "@/components/global/SideBar/SideBar";
 
 export default function HeaderSidebarContainer() {
   return (
-    <div className="w-1/4 h-screen fixed p-1 border-r-2 boder-solid boder-black">
+    <div className="w-[305px] h-screen fixed border-r-2 boder-solid boder-black overflow-scroll">
       <Header />
       <SideBar />
+      {/* 광고가 삽입되어야 함. 광고는 fixed */}
     </div>
   );
 }
