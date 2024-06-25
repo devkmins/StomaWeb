c3f6db873d0495f6c1264458c1b9b184ef3f5746
Min <esspacetoM@gmail.com>
2024-06-25 23:53:32 +0900

    edit: HeaderDonateArea 스타일 수정

--- src/components/global/Header/HeaderDonateArea.tsx
+++ src/components/global/Header/HeaderDonateArea.tsx
@@ -1,7 +1,5 @@
 import IMoney from "@/assets/icons/IMoney";
 
-import { notoSansKR } from "@/assets/fonts/notoSansKR";
-
 export default function HeaderDonateArea() {
   return (
     <div className="mt-5 flex justify-center items-center">
@@ -16,8 +14,7 @@ export default function HeaderDonateArea() {
           href={"https://buymeacoffee.com/stomaweb"}
           target="_blank"
           rel="noopener noreferrer">
-          <span
-            className={`${notoSansKR.style} font-semibold text-[14px] hover:text-gray-500`}>
+          <span className="font-semibold text-slate-700 text-[14px] hover:text-gray-500">
             응원하기!
           </span>
         </a>
