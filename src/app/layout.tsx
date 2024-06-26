9814871592eca56a28058158fbb2de43528a9c97
Min <esspacetoM@gmail.com>
2024-06-26 17:23:21 +0900

    feat: global layout에 스타일 및 Footer 컴포넌트 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,16 +3,22 @@ import "./globals.css";
 
 import HeaderSidebarContainer from "@/components/global/HeaderSidebarContainer/HeaderSidebarContainer";
 import ScrollToTopButton from "@/components/global/ScrollToTopButton/ScrollToTopButton";
+import Footer from "@/components/global/Footer/Footer";
 
 import { notoSansKR } from "@/assets/fonts/notoSansKR";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html>
-      <body className={`${notoSansKR.style} h-screen relative`}>
-        <HeaderSidebarContainer />
-        {children}
-        <ScrollToTopButton />
+    <html lang="en">
+      <body className={`${notoSansKR.style} flex h-screen relative`}>
+        <div className="w-[305px] h-full">
+          <HeaderSidebarContainer />
+        </div>
+        <div className="flex-1 flex flex-col min-h-screen">
+          <div>{children}</div>
+          <ScrollToTopButton />
+          <Footer />
+        </div>
       </body>
     </html>
   );
