55e90deca607e90ad780de7b3a9e53feaf2c2df9
Min <esspacetoM@gmail.com>
2024-07-07 23:05:55 +0900

    feat: 관심 종목 페이지의 기본  페이지 구현

--- src/app/watchlist/page.tsx
+++ src/app/watchlist/page.tsx
@@ -1,1 +1,26 @@
-export default function Watchlist() {}
+"use client";
+
+import useSetCategories from "@/hooks/useSetCategories";
+import { usePushCategoryPage } from "@/hooks/usePushCategoryPage";
+
+export default function Watchlist() {
+  const { categories } = useSetCategories();
+
+  usePushCategoryPage();
+
+  return (
+    <div>
+      {categories.length > 0 ? (
+        <div className="h-screen flex flex-col justify-center items-center pb-36">
+          <span>카테고리를 선택하여 추가한 관심 종목을 확인하세요!</span>
+        </div>
+      ) : (
+        <div className="h-screen flex flex-col justify-center items-center pb-20">
+          <span>
+            설정 아이콘을 클릭하여 카테고리를 생성하고 관심 종목을 추가하세요!
+          </span>
+        </div>
+      )}
+    </div>
+  );
+}
