8144338d96c5604f60a839db4bab10cf67c46792
Min <esspacetoM@gmail.com>
2024-07-07 23:17:59 +0900

    feat: 사이드바의 메인 항목의  하위 항목 컴포넌트 구현

---
+++ src/components/global/SideBar/SideBarSubItem.tsx
@@ -0,0 +1,74 @@
+import Link from "next/link";
+
+interface SideBarSubItemProp {
+  itemName: string;
+}
+
+export default function SideBarSubItem({ itemName }: SideBarSubItemProp) {
+  const marketsMenuItems = [
+    { href: "/markets", label: "메인" },
+    { href: "/markets/news", label: "뉴스" },
+    { href: "/markets/stocks", label: "주식" },
+    { href: "/markets/indices", label: "지수" },
+    { href: "/markets/currencies", label: "외환" },
+    { href: "/markets/futures", label: "선물" },
+    { href: "/markets/funds-etfs", label: "펀드 및 ETF" },
+    { href: "/markets/cryptocurrencies", label: "암호화폐" },
+    { href: "/markets/ipo-calendar", label: "IPO 일정" },
+  ];
+
+  const financeEducationMenuItems = [
+    { href: "/finance-education/lectures", label: "강의" },
+    { href: "/finance-education/books", label: "도서" },
+    { href: "/finance-education/services", label: "금융 관련 서비스" },
+    {
+      href: "/finance-education/banks-reports",
+      label: "전 세계 은행들의 보고서",
+    },
+  ];
+
+  const moreMenuItems = [
+    { href: "/more/about-us", label: "이 웹사이트에 대하여" },
+    { href: "/more/support", label: "문의하기" },
+    { href: "/more/terms-of-use", label: "이용약관" },
+    { href: "/more/privacy-policy", label: "개인정보 정책" },
+    { href: "/more/language-change", label: "언어 변경" },
+  ];
+
+  return (
+    <ul>
+      {itemName === "시장" &&
+        marketsMenuItems.map((item, index) => (
+          <ul key={index}>
+            <li
+              key={index}
+              className="pl-10 py-3 text-sm text-slate-500 hover:text-slate-800">
+              <Link href={item.href}>{item.label}</Link>
+            </li>
+          </ul>
+        ))}
+
+      {itemName === "투자 • 경제 공부" &&
+        financeEducationMenuItems.map((item, index) => (
+          <ul key={index}>
+            <li
+              key={index}
+              className="pl-10 py-3 text-sm text-slate-500 hover:text-slate-800">
+              <Link href={item.href}>{item.label}</Link>
+            </li>
+          </ul>
+        ))}
+
+      {itemName === "더 보기" &&
+        moreMenuItems.map((item, index) => (
+          <ul key={index}>
+            <li
+              key={index}
+              className="pl-10 py-3 text-sm text-slate-500 hover:text-slate-800">
+              <Link href={item.href}>{item.label}</Link>
+            </li>
+          </ul>
+        ))}
+    </ul>
+  );
+}
