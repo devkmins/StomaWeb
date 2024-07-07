072f587b47f47477f946a8237090e89d3127fdd5
Min <esspacetoM@gmail.com>
2024-07-07 23:19:01 +0900

    feat: 검색 페이지에 검색한  키워드를 표시

--- src/app/search/[keyword]/page.tsx
+++ src/app/search/[keyword]/page.tsx
@@ -1,1 +1,9 @@
-export default function Search() {}
+"use client";
+
+import { useParams } from "next/navigation";
+
+export default function Search() {
+  const params = useParams();
+
+  return <h1>검색: {params.keyword}</h1>;
+}
